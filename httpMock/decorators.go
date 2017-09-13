4b1d3f7c10b539e96b973e8fbcd9db195cae5a27
Dana H. P'Simer <dana_p'simer@homedepot.com>
2017-09-13 12:06:48 -0400

    removed Content-Length

--- httpMock/decorators.go
+++ httpMock/decorators.go
@@ -2,7 +2,7 @@ package httpMock
 
 import (
 	"os"
-	"fmt"
+	//"fmt"
 	"net/http"
 	"log"
 	"io"
@@ -29,7 +29,7 @@ func RespondWithJson(status int, jsonBody interface{} ) {
 	json.NewEncoder(&data).Encode(jsonBody)
 	DecorateHandler(NoopHandler, http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Add("Content-Length", fmt.Sprintf("%d", data.Len()))
+		//w.Header().Add("Content-Length", fmt.Sprintf("%d", data.Len()))
 		w.WriteHeader(status)
 		w.Write(data.Bytes())
 	}))
@@ -37,13 +37,13 @@ func RespondWithJson(status int, jsonBody interface{} ) {
 
 func RespondWithFile(status int, fileName string) {
 	DecorateHandler(NoopHandler, http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
-		stat, err := os.Stat(fileName)
-		if err != nil {
-			log.Printf("ERROR while serving up a file: %+v", err)
-			w.WriteHeader(500)
-			return
-		}
-		w.Header().Add("Content-Length", fmt.Sprintf("%d", stat.Size()))
+		//stat, err := os.Stat(fileName)
+		//if err != nil {
+		//	log.Printf("ERROR while serving up a file: %+v", err)
+		//	w.WriteHeader(500)
+		//	return
+		//}
+		//w.Header().Add("Content-Length", fmt.Sprintf("%d", stat.Size()))
 		file, err := os.Open(fileName)
 		if err != nil {
 			log.Printf("ERROR while serving up a file: %+v", err)
@@ -60,7 +60,7 @@ func RespondWithFile(status int, fileName string) {
 
 func RespondWithString(status int, body string) {
 	DecorateHandler(NoopHandler, http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
-		w.Header().Add("Content-Length", fmt.Sprintf("%d", len(body)))
+		//w.Header().Add("Content-Length", fmt.Sprintf("%d", len(body)))
 		w.WriteHeader(status)
 		w.Write([]byte(body))
 	}))
